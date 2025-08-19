d00eb94b08e99a290eb3912614aea07b116eb22e
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2025-08-19 19:18:44 +0100

    Update app.py
    comment test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Load environment variables from .env file
-
+#===#
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -52,3 +52,4 @@ def about():
 # Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
